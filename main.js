fdd6323a74f2271b2db49137d8375f69c1e5bac9
leonardo <leonardo_trifiletti@yahoo.it>
2023-11-06 21:46:42 +0100

    es 4 complete

--- main.js
+++ main.js
@@ -31,9 +31,19 @@ studente.calcola();
 studente.stampa();
 console.log(studente); */
 
-/* 3: Crea una semplice pagina HTML con un elemento div. Usa JavaScript per cambiare l'innerHTML e l'innerText del div. */
+/* 3: Crea una semplice pagina HTML con un elemento div. Usa JavaScript per cambiare l'innerHTML e l'innerText del div. 
 window.addEventListener("load", function () {
   let div = document.getElementById("cambio");
   div.innerText = "Forse ci sono riuscito";
   div.innerHTML += "<p>Ciao</p>";
-});
+}); */
+
+/* Esercizio 4: Crea una lista non ordinata con tre elementi di lista. Usa JavaScript per aggiungere un nuovo elemento di lista alla fine della lista usando appendChild. Poi usa insertBefore per aggiungere un elemento di lista all'inizio della lista. */
+
+let ul = document.getElementById("lista");
+let nuovoLiFine = document.createElement("li");
+nuovoLiFine.innerText = "4";
+ul.appendChild(nuovoLiFine);
+let nuovoLiInizio = document.createElement("li");
+nuovoLiInizio.innerText = "0";
+ul.insertBefore(nuovoLiInizio, ul.firstElementChild);
